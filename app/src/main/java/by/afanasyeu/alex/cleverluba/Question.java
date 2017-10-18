0f2ba086acbe405d9df9b735c148a1f85c4c9145
Alexei Afanasyeu <behemoth-13@yandex.ru>
2017-10-18 22:54:20 +0300

    2 stage(70 page) Quiz

---
+++ app/src/main/java/by/afanasyeu/alex/cleverluba/Question.java
@@ -0,0 +1,32 @@
+package by.afanasyeu.alex.cleverluba;
+
+/**
+ * Created by AlexPC on 18.10.2017.
+ */
+
+public class Question {
+
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int mTextResId, boolean mAnswerTrue) {
+        this.mTextResId = mTextResId;
+        this.mAnswerTrue = mAnswerTrue;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+}
