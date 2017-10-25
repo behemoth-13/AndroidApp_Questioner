0b72f848ce2c206a782c57de503a71e15af322e1
Alexei Afanasyeu <behemoth-13@yandex.ru>
2017-10-25 23:19:23 +0300

    stage5(117p)

--- app/src/main/java/by/afanasyeu/alex/cleverluba/QuizActivity.java
+++ app/src/main/java/by/afanasyeu/alex/cleverluba/QuizActivity.java
@@ -16,6 +16,7 @@ public class QuizActivity extends AppCompatActivity {
 
     private Button mTrueButton;
     private Button mFalseButton;
+    private Button mCheatButton;
     private ImageButton mNextButton;
     private ImageButton mPrevButton;
     private TextView mQuestionTextView;
@@ -55,6 +56,14 @@ public class QuizActivity extends AppCompatActivity {
             @Override
             public void onClick (View v) {
                 checkAnswer(false);
+            }
+        });
+
+        mCheatButton = (Button) findViewById(R.id.cheat_button);
+        mCheatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
             }
         });
 
