8e8b00588a6c427bd5b1adecf6cd17ce05f13812
Alexei Afanasyeu <behemoth-13@yandex.ru>
2017-11-02 23:34:31 +0300

    stage6(142p)complete

--- app/src/main/java/by/afanasyeu/alex/cleverluba/CheatActivity.java
+++ app/src/main/java/by/afanasyeu/alex/cleverluba/CheatActivity.java
@@ -1,10 +1,14 @@
 package by.afanasyeu.alex.cleverluba;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -21,6 +25,7 @@ public class CheatActivity extends AppCompatActivity {
 
     private TextView mAnswerTextView;
     private Button mShowAnswer;
+    private TextView mShowAPI;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,8 +47,33 @@ public class CheatActivity extends AppCompatActivity {
                 }
                 mIsAnswerShown = true;
                 setAnswerShownResult(mIsAnswerShown);
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    int cx = mShowAnswer.getWidth() / 2;
+                    int cy = mShowAnswer.getHeight() / 2;
+                    float radius = mShowAnswer.getWidth();
+                    Animator anim = ViewAnimationUtils.
+                            createCircularReveal(mShowAnswer, cx, cy, radius, 0);
+                    anim.addListener(new AnimatorListenerAdapter() {
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                            super.onAnimationEnd(animation);
+                            mAnswerTextView.setVisibility(View.VISIBLE);
+                            mShowAnswer.setVisibility(View.VISIBLE);
+                        }
+                    });
+                    anim.start();
+                } else {
+                    mAnswerTextView.setVisibility(View.VISIBLE);
+                    mShowAnswer.setVisibility(View.VISIBLE);
+                }
+
             }
         });
+
+        mShowAPI = (TextView) findViewById(R.id.api_text_view);
+        String apiText = "API level " + Build.VERSION.SDK_INT;
+        mShowAPI.setText(apiText);
 
         if (savedInstanceState != null) {
             mIsAnswerShown = savedInstanceState.getBoolean(KEY_ANSWER_IS_TRUE);
